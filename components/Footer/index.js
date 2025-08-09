ee0d0102dfce7788ed9eb301281be0d22f6ffa8d
Daniela Barbosab <danielalfbarbosa@gmail.com>
2025-08-09 10:27:47 +0100

    clean index to temp page and add new colors

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,19 +8,19 @@ import yourData from "../../data/content.json";
 
 const Footer = ({}) => {
   return (
-    <div className='relative bottom-0 laptop:p-0'>
-      <img
-        alt='logomarcas'
-        src='https://www.semanadecinemanegro.com.br/mostras/img/logomarcas.png'
-        style={{ width: "100%", backgroundColor: "#f9f9f9" }}
-      />
+    <div className="relative bottom-0 laptop:p-0">
+      {/* <img
+          alt='logomarcas'
+          src='https://www.semanadecinemanegro.com.br/mostras/img/logomarcas.png'
+          style={{ width: "100%", backgroundColor: "#f9f9f9" }}
+        /> */}
 
       <div
-        className='flex flex-wrap items-center justify-between p-2 mob:flex-nowrap link'
+        className="flex flex-wrap items-center justify-between p-2 mob:flex-nowrap link"
         style={{ color: "#fff", backgroundColor: "#000" }}
       >
         <span>CONTATO IMPRENSA</span>
-        <div className='flex flex-wrap mob:flex-nowrap link'>
+        <div className="flex flex-wrap mob:flex-nowrap link">
           {yourData.socials.map((social, index) => (
             <Button key={index} onClick={() => window.open(social.link)}>
               {social.title}
